018c1e65fad9f4052a5f6494c112b391a0c535dd
NguyenHuy1812 <mac@MacBook-Air-cua-Huy.local>
2019-05-28 21:19:12 +0700

    change the position

--- src/App.js
+++ src/App.js
@@ -30,6 +30,17 @@ class myGameHere extends React.Component {
       return "https://i.pinimg.com/736x/e4/ff/99/e4ff99b445b71ba5b5d2316d9295a14a--mickey-disney-mickey-hand.jpg"
     }
   }
+  renderIMGComputerChoice = () => {
+    if (this.state.computerChoice === 'rock') {
+      return "https://banner2.kisspng.com/20180705/sfh/kisspng-rockpaperscissors-computer-icons-clip-art-paper-scissors-5b3e1287be5a43.5750598415307946317797.jpg"
+    }
+    if (this.state.computerChoice === 'paper') {
+      return "https://www.pinclipart.com/picdir/middle/51-511523_rock-paper-rock-paper-scissors-clipart-png-download.png"
+    }
+    if (this.state.computerChoice === 'scissors') {
+      return "https://i.pinimg.com/736x/e4/ff/99/e4ff99b445b71ba5b5d2316d9295a14a--mickey-disney-mickey-hand.jpg"
+    }
+  }
   renderChoiceOfuser = () => {
     if (this.state.playerChoice === 'rock') {
       return ` you choose Rock & Computer Choose ${this.state.computerChoice}`
@@ -109,6 +120,7 @@ class myGameHere extends React.Component {
             <Card>
               <Card.Img style={{ width: '100%', height: '50%' }} variant="top" src="https://cdn-images-1.medium.com/max/600/1*aZYAQMfiaSiJVucrbiyKqA.gif" />
               <Card.Img style={{ width: '200px', height: '200px' }} variant="top" src={this.renderIMGChoice()} />
+              <Card.Img style={{ width: '200px', height: '200px' }} variant="top" src={this.renderIMGComputerChoice()} />
               <Card.Body>
                 <Card.Text style={{ display: "center" }}>
                   {this.renderChoiceOfuser()}
